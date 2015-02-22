f90b01811b9afa9e907176052539bcfb3158eada
Jean Chassoul <chassoul@gmail.com>
2015-02-21 20:37:40 -0600

    playing around with the ui

--- squirrel.py
+++ squirrel.py
@@ -15,23 +15,54 @@ import taskbar
 import wx
 
 
-class MainFrame(wx.Frame):
-    """
-    """
-
-    def __init__(self):
+class Colours(wx.Dialog):
+    def __init__(self, parent, id, title):
         """
             Constructor
         """
-        wx.Frame.__init__(self, None, title="Minimize to Tray")
-        panel = wx.Panel(self)
+        wx.Dialog.__init__(self, parent, id, title, size=(300, 300))        
+        
         self.tbIcon = taskbar.CustomTaskBarIcon(self)
  
         self.Bind(wx.EVT_ICONIZE, self.onMinimize)
         self.Bind(wx.EVT_CLOSE, self.onClose)
- 
-        self.Show()
- 
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        self.pnl1 = wx.Panel(self, -1)
+        self.pnl2 = wx.Panel(self, -1)
+        self.pnl3 = wx.Panel(self, -1)
+        self.pnl4 = wx.Panel(self, -1)
+        self.pnl5 = wx.Panel(self, -1)
+        self.pnl6 = wx.Panel(self, -1)
+        self.pnl7 = wx.Panel(self, -1)
+        self.pnl8 = wx.Panel(self, -1)
+
+        gs = wx.GridSizer(4,2,3,3)
+        gs.AddMany([ (self.pnl1, 0 ,wx.EXPAND),
+            (self.pnl2, 0, wx.EXPAND),
+            (self.pnl3, 0, wx.EXPAND),
+            (self.pnl4, 0, wx.EXPAND),
+            (self.pnl5, 0, wx.EXPAND),
+            (self.pnl6, 0, wx.EXPAND),
+            (self.pnl7, 0, wx.EXPAND),
+            (self.pnl8, 0, wx.EXPAND) ])
+
+        vbox.Add(gs, 1, wx.EXPAND | wx.TOP, 5)
+        self.SetSizer(vbox)
+        self.SetColors()
+        self.Centre()
+        self.Hide()
+
+    def SetColors(self):
+        self.pnl1.SetBackgroundColour(wx.BLACK)
+        self.pnl2.SetBackgroundColour(wx.Colour(139,105,20))
+        self.pnl3.SetBackgroundColour(wx.RED)
+        self.pnl4.SetBackgroundColour('#0000FF')
+        self.pnl5.SetBackgroundColour('sea green')
+        self.pnl6.SetBackgroundColour('midnight blue')
+        self.pnl7.SetBackgroundColour(wx.LIGHT_GREY)
+        self.pnl8.SetBackgroundColour('plum')
+
     def onClose(self, evt):
         """
             Destroy the taskbar icon and the frame
@@ -45,12 +76,13 @@ class MainFrame(wx.Frame):
             When minimizing, hide the frame so it "minimizes to tray"
         """
         self.Hide()
- 
+
+
 def main():
     """"""
     app = wx.App(False)
-    frame = MainFrame()
+    Colours(None, -1, 'colours.py')
     app.MainLoop()
- 
+
 if __name__ == "__main__":
     main()
--- taskbar.py
+++ taskbar.py
@@ -12,12 +12,26 @@ __author__ = 'Jean Chassoul'
 
 
 import wx
+import webbrowser
+import logging
+
+
+io_fun_website = 'http://iofun.techgcs.com'
+io_fun_help = 'http://iofun.techgcs.com/#help'
 
 
 class CustomTaskBarIcon(wx.TaskBarIcon):
     """
         Custom TaskBar Icon
     """
+    TBMENU_WEBSITE = wx.NewId()
+    TBMENU_PAUSE = wx.NewId()
+    TBMENU_RESTORE = wx.NewId()
+    TBMENU_CHANGE = wx.NewId()
+    TBMENU_REMOVE = wx.NewId()
+    TBMENU_PREFERENCES = wx.NewId()
+    TBMENU_HELP = wx.NewId()
+    TBMENU_CLOSE = wx.NewId()
 
     def __init__(self, frame):
         """
@@ -31,22 +45,84 @@ class CustomTaskBarIcon(wx.TaskBarIcon):
         self.icon = wx.EmptyIcon()
         self.icon.CopyFromBitmap(bmp)
 
-        self.SetIcon(self.icon, "Restore")
+        self.SetIcon(self.icon, "IO FUN: Squirrel monkey")
+        
+        # bind some events
+        self.Bind(wx.EVT_MENU, self.OnTaskBarClose, id=self.TBMENU_CLOSE)
+        self.Bind(wx.EVT_MENU, self.OnLaunchWebsite, id=self.TBMENU_WEBSITE)
+        self.Bind(wx.EVT_MENU, self.OnPreferences, id=self.TBMENU_PREFERENCES)
+        self.Bind(wx.EVT_MENU, self.OnHelpCenter, id=self.TBMENU_HELP)
         self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.OnTaskBarLeftClick)
- 
+        self.Bind(wx.EVT_TASKBAR_RIGHT_DOWN, self.OnTaskBarRightClick)
+
+    def CreatePopupMenu(self, evt=None):
+        """
+            This method is called by the base class when it need to popup
+            the menu for the default EVT_RIGHT_DOWN event.
+
+            Just create the menu how you want it and return it from this function,
+            the base class takes care of the rest.
+        """
+        menu = wx.Menu()
+        menu.Append(self.TBMENU_WEBSITE, "Launch Website")
+        menu.AppendSeparator()
+        menu.Append(self.TBMENU_PAUSE, "Pause Assistant")
+        menu.AppendSeparator()
+        menu.Append(self.TBMENU_PREFERENCES, "Preferences")
+        menu.Append(self.TBMENU_HELP, "Help")
+        menu.AppendSeparator()
+        menu.Append(self.TBMENU_CLOSE, "Exit")
+        return menu
+
     def OnTaskBarActivate(self, evt):
-        """"""
+        """
+
+        """
         pass
  
     def OnTaskBarClose(self, evt):
         """
             Destroy the taskbar icon and frame from the taskbar icon itself
         """
+        logging.info('on taskbar close')
         self.frame.Close()
  
     def OnTaskBarLeftClick(self, evt):
         """
+            Show the hidden squirrel
+        """
+        logging.info('on taskbar left click')
+        print('on taskbar left click')
+
+    def OnTaskBarRightClick(self, evt):
+        """
             Create the right-click menu
         """
+        logging.info('on taskbar right click')
+        print('on taskbar right click')
+        #self.frame.Hide()
+        menu = self.CreatePopupMenu()
+        self.PopupMenu(menu)
+        menu.Destroy()
+
+    def OnLaunchWebsite(self, evt):
+        """
+            Launch Website
+        """
+        print('on launch website')
+        webbrowser.open(io_fun_website)
+
+    def OnPreferences(self, evt):
+        """
+            On Preferences
+        """
+        print('on preferences')
         self.frame.Show()
         self.frame.Restore()
+
+    def OnHelpCenter(self, evt):
+        """
+            Launch Help center
+        """
+        print('on help center')
+        webbrowser.open(io_fun_help)
